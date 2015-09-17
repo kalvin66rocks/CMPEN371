6f4e3e92c22fce92091e6f7b474de49f6b13a248
kalvin66rocks <kalvin66rocks@gmail.com>
2015-09-17 12:55:20 -0400

    Lab 4 initial commit

---
+++ Lab04/Lab04_kjb5568_rjl5336/lab04_kjb5568_rjl5336.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    15:52:07 09/16/2015 
+-- Design Name: 
+-- Module Name:    lab04_kjb5568_rjl5336 - Structural 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity lab04_kjb5568_rjl5336 is
+end lab04_kjb5568_rjl5336;
+
+architecture Structural of lab04_kjb5568_rjl5336 is
+
+begin
+
+
+end Structural;
+
